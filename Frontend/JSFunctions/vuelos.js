697cd4813966ebf2809e28c8efc38c04ba21c2b8
SENA <daniel20ramirez06@gmail.com>
2025-10-22 15:02:37 -0500

    Validacion rango de fechas en vuelos

--- Frontend/JSFunctions/vuelos.js
+++ Frontend/JSFunctions/vuelos.js
@@ -7,6 +7,25 @@ let formVuelos = document.querySelector("#frmVuelo");
 let inpFechaIda = document.querySelector("#fechaIda");
 console.log(inpFechaIda)
 
+// Funcion de validacion rango
+function validarRangoFechas(inpFecha) {
+  // Validacion de fecha(hoy, a partir de 2 meses)
+  const hoy = new Date();
+
+  const fechaActualFormat = hoy.toISOString().split("T")[0];
+
+  // Establecer fecha minima
+  inpFecha.min = fechaActualFormat;
+
+  // Establecer fecha maxima
+  const fechaMax = new Date(hoy);
+  fechaMax.setMonth(hoy.getMonth() + 2);
+  const fechaMaxFormateada = fechaMax.toISOString().split("T")[0];
+  inpFecha.max = fechaMaxFormateada;
+}
+
+validarRangoFechas(inpFechaIda);
+
 btnIda.addEventListener("change", () => {
   // Si el radio button de "Ida" está seleccionado
   if (btnIda.checked) {
@@ -55,23 +74,6 @@ btnIdaVuelta.addEventListener("change", () => {
 });
 
 
-function validarRangoFechas(inpFecha) {
-  // Validacion de fecha(hoy, a partir de 2 meses)
-  const hoy = new Date();
- 
-  const fechaActualFormat = hoy.toISOString().split("T")[0];
 
-
-  // Establecer fecha minima
-  inpFecha.min = fechaActualFormat;
-
-  // Establecer fecha maxima
-  const fechaMax = new Date(hoy);
-  fechaMax.setMonth(hoy.getMonth() + 2);
-  const fechaMaxFormateada = fechaMax.toISOString().split("T")[0];
-  inpFecha.max = fechaMaxFormateada;
-}
-
-validarRangoFechas(inpFechaIda)
 
 
